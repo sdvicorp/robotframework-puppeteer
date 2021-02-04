aad3502dfd73bd35887a6e582b85fde5cdd56c53
Kurt Schuff <klschuff@gmail.com>
2021-02-04 15:48:25 -0800

    adjusting setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=[
         'robotframework>=3.2.1',
         'playwright>=0.152.0',
-        'git+https://github.com/sdvicorp/pyppeteer.git@kurt/cep-poc'
+#        'git+https://github.com/sdvicorp/pyppeteer.git@kurt/cep-poc'
 #        'pyppeteer>=0.2.2',
     ],
     # python_requires='>3.5',
